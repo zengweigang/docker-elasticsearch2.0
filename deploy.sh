f81c796bf1599b4bad71b27a673d98ff41ecfab2
zengweigang <zengweigang@gmail.com>
2015-11-23 17:34:17 +0800

    sssss Signed-off-by: zengweigang <zengweigang@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} -e zengweigang@gmail.com
 set -x
 docker push zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}
 docker tag zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}  zengweigang/daydayup:es-latest
-docker push zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}-latest
+docker push zengweigang/daydayup:es-latest
