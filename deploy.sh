c2fad8820fff41a1aa232e204a61b9618449fabe
zengweigang <zengweigang@gmail.com>
2015-11-23 16:38:53 +0800

    修改脚本 Signed-off-by: zengweigang <zengweigang@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
 docker build -t zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER} .
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} -e zengweigang@mokylin.com docker-deploy.36b.me/v2/
 docker push zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}
 docker tag zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}  zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}-latest
 zengweigang/daydayup:es-${TRAVIS_BUILD_NUMBER}-latest
